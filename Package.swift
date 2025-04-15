9c52833ed1c29ed492b9ae8213112ba58e8bad03
Théo Arrouye <5017270+tarrouye@users.noreply.github.com>
2025-04-15 11:06:49 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,10 +9,8 @@ let package = Package(
             name: "RiveRuntime",
             targets: ["RiveRuntime"])],
     targets: [
-        .binaryTarget(
-            name: "RiveRuntime",
-            url: "https://github.com/rive-app/rive-ios/releases/download/6.7.5/RiveRuntime.xcframework.zip",
-            checksum: "f109ed6359dbcfbe40c2dd2936390dbff583653e580f0c3dbc56259ea953dcf1"
+        .target(
+            name: "RiveRuntime"
         )
     ]
 )
